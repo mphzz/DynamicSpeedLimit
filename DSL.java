249b618a30c4e4758e9c2a71926cbfbba8583381
Micah <109570379+mphzz@users.noreply.github.com>
2023-01-27 12:53:23 -0800

    Update DSL.java

--- DSL.java
+++ DSL.java
@@ -1,19 +1,52 @@
+/*
+
+ORIGINAL PROTO CODE
+
+PLEASE
+
+IGNORE
+
+*/
 import java.util.Timer;
 import java.util.TimerTask;
+/*
 
+ORIGINAL PROTO CODE
+
+PLEASE
+
+IGNORE
+
+*/
 public class LightboardController {
     // function to retrieve current weather data
     public static WeatherData getWeatherData() {
         // TODO: retrieve current weather data
         return new WeatherData();
     }
+/*
 
+ORIGINAL PROTO CODE
+
+PLEASE
+
+IGNORE
+
+*/
     // function to retrieve current traffic data
     public static TrafficData getTrafficData() {
         // TODO: retrieve current traffic data
         return new TrafficData();
     }
+/*
 
+ORIGINAL PROTO CODE
+
+PLEASE
+
+IGNORE
+
+*/
     // function to calculate current speed limit based on weather and traffic conditions
     public static int calculateSpeedLimit(WeatherData weatherData, TrafficData trafficData) {
         // TODO: calculate speed limit based on weather and traffic data
@@ -25,12 +58,28 @@ public class LightboardController {
         // TODO: display speed limit on lightboard
         System.out.println("Current speed limit: " + speedLimit);
     }
+/*
 
+ORIGINAL PROTO CODE
+
+PLEASE
+
+IGNORE
+
+*/
     public static void main(String[] args) {
         // continuously update speed limit every 5 seconds
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
-            @Override
+            @Override/*
+
+ORIGINAL PROTO CODE
+
+PLEASE
+
+IGNORE
+
+*/
             public void run() {
                 // retrieve current weather and traffic data
                 WeatherData weatherData = getWeatherData();
@@ -44,4 +93,12 @@ public class LightboardController {
             }
         }, 0, 5000);
     }
-}
+}/*
+
+ORIGINAL PROTO CODE
+
+PLEASE
+
+IGNORE
+
+*/
